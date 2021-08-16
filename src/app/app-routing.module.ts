52c0355ec7ffc2504393f928c0a18c7f2a97720f
Alejandro Cruz <AlejandroCruz@users.noreply.github.com>
2021-08-16 14:27:18 -0400

    Save changes button stub

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateGenreComponent } from './genres/create-genre/create-genre.component';
 import { IndexGenresComponent } from './genres/index-genres/index-genres.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'genres', component: IndexGenresComponent}
+  {path: 'genres', component: IndexGenresComponent},
+  {path: 'genres/create', component: CreateGenreComponent}
 ];
 
 @NgModule({
--- src/app/genres/create-genre/create-genre.component.ts
+++ src/app/genres/create-genre/create-genre.component.ts
@@ -12,4 +12,7 @@ export class CreateGenreComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  saveChanges(){
+    
+  }
 }
