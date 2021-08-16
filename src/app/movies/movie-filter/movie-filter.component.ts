b2788638008657c111bb7ba6078c1a2021b1db5a
Alejandro Cruz <AlejandroCruz@users.noreply.github.com>
2021-08-16 18:46:53 -0400

    Working dropbox

---
+++ src/app/movies/movie-filter/movie-filter.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-movie-filter',
+  templateUrl: './movie-filter.component.html',
+  styleUrls: ['./movie-filter.component.css']
+})
+export class MovieFilterComponent implements OnInit {
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  form: FormGroup;
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      title: '',
+      genreId: 0,
+      upcomingReleases: false,
+      inTheaters: false
+    });
+  }
+
+}
