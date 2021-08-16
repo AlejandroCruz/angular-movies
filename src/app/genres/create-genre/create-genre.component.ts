c071907f515f96f43f43e0fb569860525a7c4b5d
Alejandro Cruz <AlejandroCruz@users.noreply.github.com>
2021-08-16 14:30:24 -0400

    Working JS navigation

--- src/app/genres/create-genre/create-genre.component.ts
+++ src/app/genres/create-genre/create-genre.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-genre',
@@ -7,12 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateGenreComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
   saveChanges(){
-    
+    this.router.navigate(['/genres'])
   }
 }
