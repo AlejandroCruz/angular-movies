26955b128bf533ddc379b69f838a8a246ca2e18c
Alejandro Cruz <AlejandroCruz@users.noreply.github.com>
2021-08-16 19:39:47 -0400

    Movie filter functionality

--- src/app/movies/movie-filter/movie-filter.component.ts
+++ src/app/movies/movie-filter/movie-filter.component.ts
@@ -13,7 +13,14 @@ export class MovieFilterComponent implements OnInit {
   form: FormGroup;
 
   genres = [{id: 1, name: 'Action'},{id: 2, name: 'Sci-Fi'}]
-
+  movies = [
+    {title: 'The Matrix', poster: 'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_QL75_UX380_CR0,4,380,562_.jpg'},
+    {title: 'Star Wars', poster: 'https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL75_UX380_CR0,14,380,562_.jpg'},
+    {title: 'Tron', poster: 'https://m.media-amazon.com/images/M/MV5BMTk4NTk4MTk1OF5BMl5BanBnXkFtZTcwNTE2MDIwNA@@._V1_QL75_UX380_CR0,0,380,562_.jpg'},
+    {title: 'Dune', poster: 'https://m.media-amazon.com/images/M/MV5BN2FjNmEyNWMtYzM0ZS00NjIyLTg5YzYtYThlMGVjNzE1OGViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_QL75_UX380_CR0,1,380,562_.jpg'},
+  ]
+  originalMovies = this.movies;
+  
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       title: '',
@@ -21,9 +28,21 @@ export class MovieFilterComponent implements OnInit {
       upcomingReleases: false,
       inTheaters: false
     });
+
+    this.form.valueChanges
+      .subscribe(values => {
+        this.movies = this.originalMovies;
+        this.filterMovies(values);
+      });
+  }
+
+  filterMovies(values: any){
+    if (values.title){
+      this.movies = this.movies.filter(movie => movie.title.indexOf(values.title) !== -1);
+    }
   }
 
   clearForm(){
-    
+    this.form.reset();
   }
 }
