1156e7dd0c71cccd919a60fa3efd57e5302d2e2e
Alejandro Cruz <AlejandroCruz@users.noreply.github.com>
2021-08-16 10:40:32 -0400

    Images to comp list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,28 +14,24 @@ export class AppComponent implements OnInit {
       this.moviesInTheaters = [{
         title: 'The Matrix',
         releaseDate: new Date('2021-01-01'),
-        price: 10.99
+        price: 10.99,
+        poster: 'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_QL75_UX380_CR0,4,380,562_.jpg'
       },
       {
         title: 'Star Wars',
         releaseDate: new Date('2021-01-02'),
-        price: 11.99
-      }];
+        price: 11.99,
+        poster: 'https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL75_UX380_CR0,14,380,562_.jpg'
+      }];  
 
       this.moviesFutureReleases = [];
-      // this.moviesFutureReleases = [{
-      //   title: 'Ironman',
-      //   releaseDate: new Date('2021-01-03'),
-      //   price: 12.99
-      // },
-      // {
-      //   title: 'Star Trek',
-      //   releaseDate: new Date('2021-01-04'),
-      //   price: 13.99
-      // }];
   }
 
   handleRating(rate:number){
     alert(`The user selected ${rate}`);
   }
 }
+/*
+Tron: 'https://m.media-amazon.com/images/M/MV5BMTk4NTk4MTk1OF5BMl5BanBnXkFtZTcwNTE2MDIwNA@@._V1_QL75_UX380_CR0,0,380,562_.jpg'
+300: 'https://m.media-amazon.com/images/M/MV5BNWMxYTZlOTUtZDExMi00YzZmLTkwYTMtZmM2MmRjZmQ3OGY4XkEyXkFqcGdeQXVyMTAwMzUyMzUy._V1_QL75_UY562_CR1,0,380,562_.jpg'
+ */
