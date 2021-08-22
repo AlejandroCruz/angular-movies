fa0e641f0852a968e118e337c5454f7a04cf73a3
Alejandro Cruz <AlejandroCruz@users.noreply.github.com>
2021-08-22 16:26:27 -0400

    Widget selected actor refine (remove)

--- src/app/actors/actors-autocomplete/actors-autocomplete.component.ts
+++ src/app/actors/actors-autocomplete/actors-autocomplete.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatAutocompleteModule, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-actors-autocomplete',
@@ -15,10 +16,13 @@ export class ActorsAutocompleteComponent implements OnInit {
   ];
   originalActors = this.actors;
   selectedActors = [];
+  columnsToDisplay = ['picture', 'name', 'character', 'actions']
 
   constructor() { }
 
   control: FormControl = new FormControl();
+
+  @ViewChild(MatTable) table: MatTable<any>;
 
   ngOnInit(): void {
     this.control.valueChanges.subscribe(value => {
@@ -31,5 +35,14 @@ export class ActorsAutocompleteComponent implements OnInit {
     console.log(event.option.value);
     this.selectedActors.push(event.option.value);
     this.control.patchValue('');
+    if(this.table !== undefined){
+      this.table.renderRows();
+    }
+  }
+
+  remove(actor){
+    const index = this.selectedActors.findIndex(a => a.name === actor.name);
+    this.selectedActors.splice(index, 1);
+    this.table.renderRows();
   }
 }
